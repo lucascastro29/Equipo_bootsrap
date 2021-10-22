e8f105ecb3fcee1d2dc5404f547827d549264b5d
lucascastro29 <lucascastro2929@gmail.com>
2021-10-22 09:56:00 -0300

    1

--- javascrip.js
+++ javascrip.js
@@ -2,3 +2,12 @@ var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggl
 var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
   return new bootstrap.Popover(popoverTriggerEl)
 })
+var toastTrigger = document.getElementById('liveToastBtn')
+var toastLiveExample = document.getElementById('liveToast')
+if (toastTrigger) {
+  toastTrigger.addEventListener('click', function () {
+    var toast = new bootstrap.Toast(toastLiveExample)
+
+    toast.show()
+  })
+}
